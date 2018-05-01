e31b7021844ba800a12bde5b141250b645c0e4de
Felix <ff@rapidfacture.com>
2018-05-01 22:37:24 +0200

    rf logo

--- index.js
+++ index.js
@@ -155,7 +155,9 @@ function configure (options, force) {
 
 function printInstallationHeader (path) {
    console.log('\n');
-   log.info('RAPIDFACTURE');
+   var rfBig = '   _______   ___    _   ___ ___ ___  ___  _   ___ _____ _   _ ___ ___\n  /       / | _ \\  /_\\ | _ \\_ _|   \\| __|/_\\ / __|_   _| | | | _ \\ __|\n /  ( )  /  |   / / _ \\|  _/| || |) | _|/ _ \\ (__  | | | |_| |   / _| \n/_______/   |_|_\\/_/ \\_\\_| |___|___/|_|/_/ \\_\\___| |_|  \\___/|_|_\\___|';
+   // var rfSmall = '   _______    _  _  _ ___ _   __ _  __ ___    _  __\n  /       /  |_)|_||_) | | \\ |_ |_|/    | | ||_)|_ \n /  ( )  /   | \\| ||  _|_|_/ |  | |\\__  | |_|| \\|__ \n/______ /';
+   log.info(rfBig);
    log.info('-----------------------------------------');
    log.info(packageJson.name + ' Installation');
    log.info(packageJson.description);
