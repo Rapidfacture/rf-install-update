32dc9edc691bff63f97800f7bdd3d6944605f29f
Felix <ff@rapidfacture.com>
2018-05-02 01:40:59 +0200

    fix index file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-   install: require('.lib/install.js'),
-   update: require('.lib/update.js')
+   install: require('./lib/install.js'),
+   update: require('./lib/update.js')
 };
